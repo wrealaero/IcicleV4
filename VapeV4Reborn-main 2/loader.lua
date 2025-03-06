0cd1f119b04a619097fe961d31f47d568e875f4e
wrealaero <clanwarobi@gmail.com>
2025-03-06 17:52:55 -0600

    Delete VapeV4Reborn-main 2 directory

--- VapeV4Reborn-main 2/loader.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ImDamc/VapeV4Reborn/refs/heads/main/main.lua", true))()
-queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/ImDamc/VapeV4Reborn/refs/heads/main/main.lua", true))()')
