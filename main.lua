a787c886da92001fc16be5e9b4a343df28e0f3c2
wrealaero <clanwarobi@gmail.com>
2025-03-06 20:09:42 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -65,6 +65,11 @@ ConfirmButton.MouseButton1Click:Connect(function()
     if enteredKey == key then
         getgenv().keyCorrect = true -- Prevents GUI from coming back
         ScreenGui:Destroy() -- Removes GUI
+        game.StarterGui:SetCore("SendNotification", {
+            Title = "Correct Key!";
+            Text = "Join .gg/icicle for more updates!";
+            Duration = 5;
+        })
         task.wait(0.5) -- Small delay to prevent issues
         loadstring(game:HttpGet("https://raw.githubusercontent.com/wrealaero/IcicleV4/refs/heads/main/main.lua", true))()
     else
@@ -78,119 +83,47 @@ end)
 
 repeat task.wait() until getgenv().keyCorrect -- Wait until correct key is entered
 
--- Continue executing your existing script
-if identifyexecutor then
-	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
-		getgenv().setthreadidentity = nil
-	end
-end
+-- Prevent loading the script again
+if not getgenv().vapeLoaded then
+    getgenv().vapeLoaded = true
 
-local vape
-local loadstring = function(...)
-	local res, err = loadstring(...)
-	if err and vape then
-		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
-	end
-	return res
-end
-local queue_on_teleport = queue_on_teleport or function() end
-local isfile = isfile or function(file)
-	local suc, res = pcall(function()
-		return readfile(file)
-	end)
-	return suc and res ~= nil and res ~= ''
-end
-local cloneref = cloneref or function(obj)
-	return obj
-end
-local playersService = cloneref(game:GetService('Players'))
+    -- Continue executing your existing script
+    if identifyexecutor then
+        if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
+            getgenv().setthreadidentity = nil
+        end
+    end
 
-local function downloadFile(path, func)
-	if not isfile(path) then
-		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/ImDamc/VapeV4Reborn/refs/heads/main/'..'/'..select(1, path:gsub('newvape/', '')), true)
-		end)
-		if not suc or res == '404: Not Found' then
-			error(res)
-		end
-		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
-		end
-		writefile(path, res)
-	end
-	return (func or readfile)(path)
-end
+    local vape
+    local loadstring = function(...)
+        local res, err = loadstring(...)
+        if err and vape then
+            vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+        end
+        return res
+    end
 
-local function finishLoading()
-	vape.Init = nil
-	vape:Load()
-	task.spawn(function()
-		repeat
-			vape:Save()
-			task.wait(10)
-		until not vape.Loaded
-	end)
+    local playersService = game:GetService('Players')
 
-	local teleportedServers
-	vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
-		if (not teleportedServers) and (not shared.VapeIndependent) then
-			teleportedServers = true
-			local teleportScript = [[
-				repeat task.wait() until game.HttpGet ~= nil
+    local function finishLoading()
+        vape.Init = nil
+        vape:Load()
+        task.spawn(function()
+            repeat
+                vape:Save()
+                task.wait(10)
+            until not vape.Loaded
+        end)
+    end
 
-				shared.vapereload = true
-				if shared.VapeDeveloper then
-					loadstring(readfile('newvape/loader.lua'), 'loader')()
-				else
-					loadstring(game:HttpGet("https://raw.githubusercontent.com/wrealaero/IcicleV4/refs/heads/main/main.lua", true))()
-				end
-			]]
-			if shared.VapeDeveloper then
-				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
-			end
-			if shared.VapeCustomProfile then
-				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
-			end
-			vape:Save()
-			queue_on_teleport(teleportScript)
-		end
-	end))
+    vape = loadstring(game:HttpGet('https://raw.githubusercontent.com/ImDamc/VapeV4Reborn/refs/heads/main/guis/default.lua'))()
+    shared.vape = vape
 
-	if not shared.vapereload then
-		if not vape.Categories then return end
-		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
-			vape:CreateNotification('Finished Loading', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
-		end
-	end
-end
-
-if not isfile('newvape/profiles/gui.txt') then
-	writefile('newvape/profiles/gui.txt', 'new')
-end
-local gui = readfile('newvape/profiles/gui.txt')
-
-if not isfolder('newvape/assets/'..gui) then
-	makefolder('newvape/assets/'..gui)
-end
-vape = loadstring(downloadFile('newvape/guis/'..gui..'.lua'), 'gui')()
-shared.vape = vape
-
-if not shared.VapeIndependent then
-	loadstring(downloadFile('newvape/games/universal.lua'), 'universal')()
-	if isfile('newvape/games/'..game.PlaceId..'.lua') then
-		loadstring(readfile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
-	else
-		if not shared.VapeDeveloper then
-			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/ImDamc/VapeV4Reborn'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
-			end)
-			if suc and res ~= '404: Not Found' then
-				loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
-			end
-		end
-	end
-	finishLoading()
-else
-	vape.Init = finishLoading
-	return vape
+    if not shared.VapeIndependent then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ImDamc/VapeV4Reborn/refs/heads/main/games/universal.lua", true))()
+        finishLoading()
+    else
+        vape.Init = finishLoading
+        return vape
+    end
 end
